4b6bcca8530b07966fbfe066f41dd92f6cb75b1f
Durgesh2398 <2308durgesh@gmail.com>
2020-02-23 23:48:04 +0530

    first

---
+++ NumberOfWaysDistance.java
@@ -0,0 +1,23 @@
+package dp;
+
+import java.util.Scanner;
+
+public class NumberOfWaysDistance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println(output(n));
+	}
+	static int output(int n) {
+		if(n<0) {
+			return 0;
+		}
+		if(n==0) {
+			return 1;
+		}
+		return output(n-1)+output(n-2)+output(n-3);
+	}
+
+}
